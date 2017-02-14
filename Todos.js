c9dacba46b569c292a29e62ca15f0865b532da1a
Oaker Min <Brucetheflame@outlook.com>
2017-02-14 16:57:45 +0800

    Updated with comments for each function

--- Todos.js
+++ Todos.js
@@ -1,19 +1,23 @@
 var todos = ["item 1", "item 2", "item 3"];
 
+//It should have a function to display todos
 function displayTodo(){
     console.log("My todos: " + todos);
 }
 
+//It should have a function to add todos
 function addTodos(value){
     todos.push(value);
     displayTodo();
 }
 
+//It should have a function to change todos.
 function changeTodos(position, value){
     todos[position] = value;
     displayTodo();
 }
 
+//It should have a function to delete todos.
 function deleteTodos(del){
     todos.splice(del,1);
     displayTodo();
